135ebb705c66c0e1b2effb1b1999045a28d06ec9
rohit-jaiswal2 <rohit.jaiswal2@hpe.com>
2018-02-23 15:28:11 +0530

    add thirst print

--- hello.py
+++ hello.py
@@ -2,5 +2,8 @@ hungry=raw_input("are you hungry:")
 if hungry=='yes':
     print "eat samosa"
     print "eat namkeen"
+    print "fries"
 else:
+    print "if u are thristy then do"
+    print "drink soda"
     print "do your work"
