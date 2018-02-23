be084df63603120971ac55c785e3147208aa2e4a
rohit-jaiswal2 <rohit.jaiswal2@hpe.com>
2018-02-23 15:54:52 +0530

    Revert "umcommit"
    This reverts commit 4711803d9fcc47aa9a5cddbb4960cabf601a264c.

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ if hungry=='yes':
     print "eat samosa"
     print "eat namkeen"
     print "fries"
-    print "uncommit changes"
 else:
     print "if u are thristy then do"
     print "drink soda"
